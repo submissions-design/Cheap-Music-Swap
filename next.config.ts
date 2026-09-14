214aa9eed035fe421bab6a9551f57146c6131093
submissions-design <submissions@getnffunding.com>
2026-09-14 13:48:34 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-2b06c84d72134bd8ab9357cd35d34199.r2.dev",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
