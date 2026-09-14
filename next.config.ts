087064930b27e2640bd619b39e2dac487b0c417d
submissions-design <submissions@getnffunding.com>
2026-09-14 09:57:41 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    cpus: 1,
-    workerThreads: false,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
